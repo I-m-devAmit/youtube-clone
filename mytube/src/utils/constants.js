61a104056b2bec5ed01ec0adb0eab7ddbd71c378
AMIT SHARMA <amitsharma1910@outlook.com>
2023-02-15 21:42:16 +0530

    HOC + Debouncing + Caching Search + Nested Comments

--- mytube/src/utils/constants.js
+++ mytube/src/utils/constants.js
@@ -2,6 +2,7 @@ const Google_API_KEY = "AIzaSyDUnP1uyiDGnpL-Zi29BpZ6S3Lk_RAgStQ";
 
 export const YOUTUBE_VIDEOS_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=' + Google_API_KEY;
 
-//
+export const YOUTUBE_SEARCH_API = 'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
+
 // Authorization: Bearer[YOUR_ACCESS_TOKEN]
 // Accept: application / json
